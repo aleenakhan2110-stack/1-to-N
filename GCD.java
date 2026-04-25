3493168aa6e11efc6f52d18ea49bd333e9215e6f
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-25 22:54:55 +0530

    Add files via upload

--- GCD.java
+++ GCD.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 public class GCD {
      public static void main(String[] args) {
          Scanner sc=new Scanner(System.in);
-         int N=sc.nextInt();
+         if(sc.hasNextInt()){
+             int A=sc.nextInt();
+             int B=sc.nextInt();
+             while(B!=0){
+                 int temp=B;
+                 B=A%B;
+                 A=temp;
+             }
+             System.out.println(A);
+
+         }
+
+
+
 
 
     }
