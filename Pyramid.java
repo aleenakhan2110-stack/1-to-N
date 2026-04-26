f9014f72103aa162460391091c9ef692bdb3d01e
aleenakhan2110-stack <aleenakhan2110@gmail.com>
2026-04-26 22:33:19 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Pyramid {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         if(sc.hasNextInt()) {
+             int N = sc.nextInt();
+             for (int i = 1; i <= N; i++) {
+                 for (int j = 1; j <= i; j++) {
+                     System.out.println("*");
+                 }
+                 System.out.println();
+             }
+         }
+
+
+    }
+}
